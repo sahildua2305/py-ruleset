3edac76d321b205f1713e72de080f10777db0c7a
Sahil Dua <sahil.dua@booking.com>
2018-01-21 17:32:45 +0100

    ruleset: add documentation for both RuleSet and Options classes

--- ruleset/Options.py
+++ ruleset/Options.py
@@ -1,26 +1,52 @@
 class Options:
+    """
+    Options represents the state of RuleSet and enables selection and
+    deselection of options in the RuleSet. It maintains a set of selected
+    options.
+
+    Attributes:
+        rs: A RuleSet object to use.
+        selectedOpts: A set of selected options at any point.
+    """
 
     def __init__(self, rs):
+        """
+        Returns an empty Options class with a given RuleSet object.
+
+        :param rs: (RuleSet) RuleSet object to use for this Options object
+        """
         self.rs = rs
         self.selectedOpts = set()
 
     def toggle(self, option):
-        # print "start toggle:", self.selectedOpts
+        """
+        Implements functionality for toggling any option. It respects all
+        the rules defined in the RuleSet object while selecting or deselecting
+        any option.
+
+        :param option: (str) option to toggle
+        :return: None
+        """
         if option not in self.selectedOpts:
             # dfs on option and select all visited options
             dfsPath = list(self.rs.forwardDfs(option))
             for dfsOpt in dfsPath:
                 self.selectedOpts.add(dfsOpt)
                 # get all conflicting options for dfsOpt
-                conflicts = self.rs.getConflicts(dfsOpt)
-                # print dfsOpt, conflicts
+                conflicts = self.rs.conflicts[dfsOpt]
                 for conflict in conflicts:
-                    self.__removeOption(conflict)
+                    self.__deselectOption(conflict)
         else:
-            self.__removeOption(option)
-        # print "end toggle:", self.selectedOpts
+            self.__deselectOption(option)
 
-    def __removeOption(self, option):
+    def __deselectOption(self, option):
+        """
+        Recursive implementation to deselect options based on the rules
+        defined in the RuleSet object.
+
+        :param option: (str) option to start the recursion from
+        :return: None
+        """
         # reverse dfs on option and deselect all visited options
         reverseDfsPath = list(self.rs.reverseDfs(option))
         # if len(reverseDfsPath) <= 1:
@@ -30,4 +56,9 @@ class Options:
                 self.selectedOpts.remove(dfsOpt)
 
     def selection(self):
+        """
+        Returns the `selectedOpts` for the Options object.
+
+        :return: (set) selected options
+        """
         return self.selectedOpts
--- ruleset/RuleSet.py
+++ ruleset/RuleSet.py
@@ -1,28 +1,64 @@
 class RuleSet:
+    """
+    A RuleSet represents a set of rules between the options. RuleSets have
+    following properties:
+
+    Attributes:
+        deps: A dictionary representing the dependencies between the options.
+        This is a graph representation as a adjacency list.
+        reverseDeps: A dictionary representing the reverseDependencies between
+        the options. This is used to traverse backwards to unselect options
+        on which unselected options depend.
+        conflicts: A dictionary representing the conflicts between the options.
+    """
 
     def __init__(self):
-        self.allOptions = set()
+        """
+        Returns a RuleSet object and initializes deps, reverseDeps and
+        conflicts as empty dictionaries.
+        """
         self.deps = dict()
         self.reverseDeps = dict()
         self.conflicts = dict()
 
     def addDep(self, option1, option2):
+        """
+        Adds a dependency between option1 and option2 to the RuleSet object.
+
+        :param option1: (str) first option which depends on the second option
+        :param option2: (str) second option on which first option depends
+        :return: None
+        """
         self.__addNewOptions(option1, option2)
         self.deps[option1].add(option2)
         self.reverseDeps[option2].add(option1)
 
     def addConflict(self, option1, option2):
+        """
+        Adds a confict between option1 and option2 to the RuleSet object.
+
+        :param option1: (str) first option
+        :param option2: (str) second option
+        :return: None
+        """
         self.__addNewOptions(option1, option2)
         self.conflicts[option1].append(option2)
         self.conflicts[option2].append(option1)
 
     def isCoherent(self):
-        allOptions = list(self.allOptions)
+        """
+        Checks if the RuleSet object is coherent, that is, that no option can
+        depend, directly or indirectly, on another option and also be mutually
+        exclusive with it.
+
+        :return: True if the RuleSet object is coherent, False otherwise.
+        """
+        allOptions = self.deps.keys()
         for option in allOptions:
-            # make list of deps for option
+            # Get all dependencies for option
             deps = list(self.forwardDfs(option))
 
-            # make sure option doesn't have conflict with any of its deps
+            # Check if option doesn't have conflict with any of its deps
             for dep in deps:
                 if dep != option and dep in self.conflicts[option]:
                     return False
@@ -36,11 +72,15 @@ class RuleSet:
         return True
 
     def __addNewOptions(self, option1, option2):
-        if option1 not in self.allOptions:
-            self.allOptions.add(option1)
-        if option2 not in self.allOptions:
-            self.allOptions.add(option2)
+        """
+        Adds any new options to deps, reverseDeps and conflicts dictionaries
+        so that we always have all the keys present in all of these three
+        data structures.
 
+        :param option1: (str) first option
+        :param option2: (str) second option
+        :return: None
+        """
         if option1 not in self.deps:
             self.deps[option1] = set()
         if option2 not in self.deps:
@@ -57,12 +97,31 @@ class RuleSet:
             self.conflicts[option2] = list()
 
     def forwardDfs(self, start):
+        """
+        Implements Depth-First Search (DFS) on the basis of `deps` dictionary.
+
+        :param start: (str) starting option for Depth-First Search
+        :return: Set of options present in the DFS traversal from start option
+        """
         return self.__dfs(self.deps, start)
 
     def reverseDfs(self, start):
+        """
+        Implements Depth-First Search (DFS) on the basis of `reverseDeps` dictionary.
+
+        :param start: (str) starting option for Depth-First Search
+        :return: Set of options present in the DFS traversal from start option
+        """
         return self.__dfs(self.reverseDeps, start)
 
     def __dfs(self, graph, start):
+        """
+        Implements Depth-First Search (DFS) on the basis of given graph.
+
+        :param graph: (dict) dictionary containing graph representation
+        :param start: (str) starting option for Depth-First Search
+        :return: Set of options present in the DFS traversal from start option
+        """
         visited, stack = set(), [start]
         while stack:
             vertex = stack.pop()
@@ -70,6 +129,3 @@ class RuleSet:
                 visited.add(vertex)
                 stack.extend(graph[vertex] - visited)
         return visited
-
-    def getConflicts(self, option):
-        return self.conflicts[option]
